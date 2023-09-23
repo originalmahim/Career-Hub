cf1f8ad5380c61abe97a851035974dfb96a26fb3
originalmahim <tareqazizmahim@gmail.com>
2023-09-24 02:02:10 +0600

    Worked on banner image and h1 text, responsive for all large devices only

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,10 +3,16 @@
 
 const Banner = () => {
           return (
-          <div className="bg-gradient-to-r from-violet-100 via-white to-violet-100">
-          <div className="max-w-7xl mx-auto lg:h-[60vh] md:h-[30vh] border-none rounded-md mb-5 px-3">
-            <h1>Banner</h1>                 
-          </div>
+          <div className="bg-gradient-to-r from-violet-100 via-white to-violet-100" >
+          <div className="max-w-7xl mx-auto lg:h-[60vh] md:h-[30vh] border-none rounded-md mb-5 px-3 bg-no-repeat bg-right" style={{ backgroundImage: "url('https://i.ibb.co/0YkrMGM/user.png')", backgroundSize: '620px 580px', }}>
+  <div className="flex items-center h-full">
+    <h1 className="text-left lg:text-7xl font-bold">
+      One Step <br />
+      Closer To Your <br />
+      <span className="text-violet-400">Dream Job</span>
+    </h1>
+  </div>
+</div>
           </div>
           );
 };
